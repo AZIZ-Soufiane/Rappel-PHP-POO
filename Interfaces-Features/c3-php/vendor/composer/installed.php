2ebeda57524228318dffdb904d82bef9712b5764
AZIZ-Soufiane <aziz.soufiane.solicode@gmail.com>
2025-09-18 15:58:35 +0100

    Interfaces & Features

---
+++ Interfaces-Features/c3-php/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'solicode/c3-php',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c2e40e90d774eb67f63c9441e96b10274c365a5a',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'solicode/c3-php' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c2e40e90d774eb67f63c9441e96b10274c365a5a',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
